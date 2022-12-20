41533b4b5b6d548c60b466f28b1adeda41f2f2a1
BACHTUVU\PC <bachtuvu.dev@gmail.com>
2022-12-20 23:05:02 +0700

    up

--- authorization-server/src/main/java/com/ansv/authorizationserver/service/impl/rabbitmq/RabbitMqSender.java
+++ authorization-server/src/main/java/com/ansv/authorizationserver/service/impl/rabbitmq/RabbitMqSender.java
@@ -1,38 +1,38 @@
-//package com.ansv.authorizationserver.service.impl.rabbitmq;
-//
-//import com.ansv.authorizationserver.dto.response.UserDTO;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.springframework.amqp.core.AmqpTemplate;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//
-//
-//@Service
-//public class RabbitMqSender {
-//
-//    @Autowired
-//    private AmqpTemplate rabbitTemplate;
-//
-//    @Value("${spring.rabbitmq.exchange:#{null}}")
-//    private String exchange;
-//
-//    @Value("${spring.rabbitmq.routingkey:#{null}}")
-//    private String routingkey;
-//
-//
-//    public void sender(UserDTO user) {
-//
-//        rabbitTemplate.convertAndSend(exchange, routingkey, user
-//        );
-//    }
-//
-//    public void sender(String username) {
-//        rabbitTemplate.convertAndSend(exchange, routingkey, username);
-//    }
-//
-//}
+package com.ansv.authorizationserver.service.impl.rabbitmq;
+
+import com.ansv.authorizationserver.dto.response.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RabbitMqSender {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Value("${spring.rabbitmq.exchange:#{null}}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.routingkey:#{null}}")
+    private String routingkey;
+
+
+    public void sender(UserDTO user) {
+
+        rabbitTemplate.convertAndSend(exchange, routingkey, user
+        );
+    }
+
+    public void sender(String username) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, username);
+    }
+
+}
