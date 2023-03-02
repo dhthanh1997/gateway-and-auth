a2f11f95f17ccec9b7ef5d4fa8cb705a2881c904
BACHTUVU\PC <bachtuvu.dev@gmail.com>
2023-03-02 15:01:06 +0700

    update

--- authorization-server/src/main/java/com/ansv/authorizationserver/AuthorizationConfig.java
+++ authorization-server/src/main/java/com/ansv/authorizationserver/AuthorizationConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AuthorizationConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("*")
+        registry.addMapping("/authorization").allowedMethods("*").allowedOriginPatterns("http://localhost:8055")
                 .allowCredentials(true);
     }
 }
