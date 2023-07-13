4984b15de8cf11322a9d270fa278c6c791fc46d2
LAPTOP-2CP7E18J\ACER <swordartonline1308@gmail.com>
2023-07-13 16:57:42 +0700

    up

--- gateway/src/test/java/com/ansv/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/ansv/gateway/GatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.ansv.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatewayApplicationTests.class)
 class GatewayApplicationTests {
 
 	@Test
